ecae298a3b23e35351dd178055b3e230a0e0da07
Deivid Santos <deividsantos121@gmail.com>
2022-11-22 15:51:44 -0300

    Classe Date

---
+++ Date.h
@@ -0,0 +1,25 @@
+#ifndef DATE_H
+#define DATE_H
+#include <iostream>
+#include <string>
+
+class Date {
+
+    private:
+        int dia, mes, ano;
+    public:
+        Date();
+
+        int getDia() { return this->dia; }
+        void setDia(int dia) { this->dia = dia; }
+        
+        int getMes() { return this->mes; }
+        void setMes(int mes) { this->mes = mes; }
+
+        int getAno() { return this->ano; }
+        void setAno(int ano) { this->ano = ano; }
+
+        void print() { cout << this->getDia() << "/" << this->getMes() << "/" << this->getAno() << endl; }
+
+};
+#endif // DATE_H
