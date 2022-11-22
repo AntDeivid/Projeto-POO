74b5d9bec596797b8da824ab96b257c3c03f21a5
Victor Emanuel de Sousa Costa <victorwmanuel.costa@alu.ufc.br>
2022-11-22 14:47:05 -0300

    Add files via upload
    Classe Jogador Defesa (Prototipação e Implementação)

---
+++ jogadorDefensa.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "jogadorDefesa.h"
+
+using namespace std;
+
+JogadorDefesa::JogadorDefesa(String nome, int idade, int habilidade, int gols, int camisa, int cobertura, int desarme) : Jogador(nome, idade, habilidade, gols, camisa)
+{
+    this->cobertura = cobertura;
+    this->desarme = desarme;
+}
+
+int JogadorDefesa::getHabilidade()
+{
+    return ((habilidade*5) + (cobertura*3) + (desarme*2) )/10;
+}
