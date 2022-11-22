0b3dcea26f26f216db76ed46f11e7da4e4bb57b2
Jotape003 <jpedrosm2704@gmail.com>
2022-11-22 16:18:18 -0300

    modificar

--- Time.cpp
+++ Time.cpp
@@ -14,6 +14,12 @@ void Time::setNome(string nome){
     this->nome = nome;
 }
 
+void Time::printJogadores(Jogador *jogador[]){
+    for(int i=0; i<11; i++){
+        cout << jogador[i]->getNome() << endl;
+    }
+}
+
 
 string Time::getResultados(){
     cout << "Vitorias: " << somaVitorias() << ", Empates: " << somaEmpates() << ", Derrotas: " << somaDerrotas() << endl;
--- Time.hpp
+++ Time.hpp
@@ -7,20 +7,20 @@ class Time : public Jogador{
     private:
         std::string nome;
         Jogador *jogadores;
-        int vitorias;
-        int derrotas;
-        int empates;
+        int vitorias{0};
+        int derrotas{0};
+        int empates{0};
     
     public:
         Time(std::string nome);
         std::string getNome();
         void setNome(std::string nome);
         std::string getResultados();
-        void printJogadores();
+        void printJogadores(Jogador *jogador[]);
         int somaVitorias();
         int somaDerrotas();
         int somaEmpates();
-        Jogador insJogadores(Jogador *jogador);
+        void insJogadores(Jogador *jogador);
 };
 
 #endif // TIME_H
