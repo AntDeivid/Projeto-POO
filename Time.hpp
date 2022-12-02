5f90ddeedb349ef1f40c998d86ca783fdbbfe82f
Jotape003 <jpedrosm2704@gmail.com>
2022-12-01 21:07:36 -0300

    modificar

--- Time.hpp
+++ Time.hpp
@@ -8,12 +8,8 @@
 class Time : public Jogador{
     private:
         std::string nome;
-<<<<<<< HEAD
-        vector<Jogador> jogador;
-=======
         std::vector <Jogador> jogador;
         int numJog{0};
->>>>>>> 9db50ed42ab36e6ee7b2d94d9180f57cf8fcff58
         int vitorias{0};
         int derrotas{0};
         int empates{0};
